08e38adac549e07efe1997659ccfb078e2156e8e
mystyfly <mystyfly@gmail.com>
2014-03-09 10:33:41 +0100

    another big adjustment, owing to the fact that distance and speed must be recalculated if a collision occurs. WIP.

--- Squash Simulation/src/ch/squash/simulation/shapes/common/Movable.java
+++ Squash Simulation/src/ch/squash/simulation/shapes/common/Movable.java
@@ -10,21 +10,22 @@ import ch.squash.simulation.shapes.shapes.Quadrilateral;
 public class Movable {
 	// static
 	private final static String TAG = Movable.class.getSimpleName();
-
+	private final float SPECIFIC_WARMTH_CAPACITY;
+	
 	// shapes
 	private final AbstractShape mShape;
 	public final PhysicalVector[] vectorArrows;
 	public final PhysicalVector gravitation;
 	public final PhysicalVector speed;
 	public final PhysicalVector airFriction;
-	public final Trace mTrace;
+	public final Trace trace;
 
 	// data for between movements
-	private Collision lastMovementCollision;
+	private Collision mLastMovementCollision;
 	private long mNextMovement = System.currentTimeMillis(); // ms
-	private int moveSkipCount;
+	private int mMoveSkipCount;
 
-	public Movable(final AbstractShape shape, final float[] origin) {
+	public Movable(final AbstractShape shape, final float specificWarmthCapacity, final float[] origin) {
 		mShape = shape;
 		gravitation = new PhysicalVector("force_gravitation", origin.clone(),
 				getGravitation().getDirection(), new float[] { 0, 0.5f, 0, 1 });
@@ -35,8 +36,10 @@ public class Movable {
 
 		vectorArrows = new PhysicalVector[] { gravitation, speed, airFriction };
 
-		mTrace = new Trace(shape.tag + "\'s trace", new float[] { 0.35f, 0.35f,
+		trace = new Trace(shape.tag + "\'s trace", new float[] { 0.35f, 0.35f,
 				0.35f, 1 });
+		
+		SPECIFIC_WARMTH_CAPACITY = specificWarmthCapacity;
 	}
 
 	public static IVector getGravitation() {
@@ -70,122 +73,126 @@ public class Movable {
 
 		if (sleep < -MovementEngine.DELAY_BETWEEN_MOVEMENTS) {
 			Log.e(TAG, "Skipping move");
-			moveSkipCount++;
+			mMoveSkipCount++;
 		} else {
-			move((moveSkipCount + 1) * MovementEngine.DELAY_BETWEEN_MOVEMENTS
+			move((mMoveSkipCount + 1) * MovementEngine.DELAY_BETWEEN_MOVEMENTS
 					/ 1000f * Settings.getSpeedFactor());
-			moveSkipCount = 0;
+			mMoveSkipCount = 0;
 		}
 	}
 
 	// move in seconds to use Si-units
 	private void move(final float dt) {
-		// distance that is travelled if speed is considered to be constant
-		// during dt
-		final IVector distance = speed.multiply(dt);
-
-		final boolean rolling = isRolling();
-		lastMovementCollision = null;
-
-		for (final AbstractShape solid : SquashRenderer.getCourtSolids()) {
-			final Collision collision = Collision.getCollision(mShape,
-					distance, solid, lastMovementCollision);
-
-			if (collision != null){
-				if (collision.travelPercentage < 0)
-					Log.e(TAG, "travelperc of " + solid.tag + " and ball is " + collision.travelPercentage);
+		final IVector oldSpeed = speed.multiply(1);
 		
-				if (lastMovementCollision == null)
-					lastMovementCollision = collision;
-				else if (collision.travelPercentage < lastMovementCollision.travelPercentage)
-					lastMovementCollision = collision;
+		if (isRolling()){
+//			mLastMovementCollision = null;
+			// TODO
+			Log.w(TAG, "Ball is ROLLING which is NOT IMPLEMENTED");
+		}else{
+			//////////////////////////////////////
+			// calculating distance travelled
+			//////////////////////////////////////
+			// calculate distance with old speed
+			final IVector oldDistance = speed.multiply(dt);
+
+			// set forces
+			airFriction.setDirection(speed.getNormalizedVector().multiply(
+					-((Ball)mShape).getDragFactor() * speed.getLength() * speed.getLength()));
+			
+			// calculate acceleration
+			final IVector acceleration = airFriction.add(gravitation);
+
+			// update speed: v = v0 + a*t
+			speed.setDirection(speed.add(acceleration.multiply(dt)));
+
+			// calculate actual distance travelled by taking the average of old and new distance
+			final IVector actualDistance = (speed.multiply(dt).add(oldDistance)).multiply(0.5f);
+
+			//////////////////////////////////////
+			// detecting collisions 
+			//////////////////////////////////////
+			Collision possibleCollision = null;
+			for (final AbstractShape solid : SquashRenderer.getCourtSolids()) {
+				final Collision collision = Collision.getCollision(mShape,
+						actualDistance, solid, mLastMovementCollision);
+
+				if (collision != null){
+					if (collision.travelPercentage < 0)
+						Log.e(TAG, "travelperc of " + solid.tag + " and ball is " + collision.travelPercentage);
+			
+					if (possibleCollision == null)
+						possibleCollision = collision;
+					else if (collision.travelPercentage < possibleCollision.travelPercentage)
+						possibleCollision = collision;
+				}
+			}
+			mLastMovementCollision = possibleCollision;
+			
+			//////////////////////////////////////
+			// reacting to collision
+			//////////////////////////////////////
+			if (mLastMovementCollision == null){
+				// complete movement "as planned
+				// move shape
+				mShape.move(actualDistance);
+				
+				// increase temperature based on air friction
+				final float airFrictionLength = airFriction.getLength();
+				mShape.temperature += (speed.getLength() * airFrictionLength * dt +
+						0.5f * airFrictionLength * airFrictionLength * dt * dt) / SPECIFIC_WARMTH_CAPACITY;
+			}else{
+				final float curDt = dt * mLastMovementCollision.travelPercentage;
+				final float newDt = dt - curDt;
+				
+				// MUST NO LONGER USE ACTUALDISTANCE!!
+				
+				// adjust speed update
+				speed.setDirection(oldSpeed.add(acceleration.multiply(curDt)));
+				// move shape
+				mShape.moveTo(mLastMovementCollision.collisionPoint);
+				
+				// increase temperature based on air friction
+				final float airFrictionLength = airFriction.getLength();
+				mShape.temperature += (speed.getLength() * airFrictionLength * curDt +
+						0.5f * airFrictionLength * airFrictionLength * curDt * curDt) / SPECIFIC_WARMTH_CAPACITY;
+				
+				
+				// do collision stuff
+				MovementEngine.playSound(mLastMovementCollision.collidedSolid.tag);
+				
+				// calculate ausfallswinkel (= einfallswinkel, must change that)
+				final IVector n = mLastMovementCollision.solidNormalVector.getNormalizedVector();
+				
+				final IVector newSpeed = speed.add(n.multiply(-2 * speed.multiply(n))).multiply(Settings.getCoefficientOfRestitution()); // formula for ausfallswinkel
+				
+				// increase temperature based on collision
+				mShape.temperature += (float) (0.5f * (Math.pow(speed.getLength(), 2) - Math.pow(newSpeed.getLength(), 2)) / SPECIFIC_WARMTH_CAPACITY);
+				
+				speed.setDirection(newSpeed);
+				
+				move(newDt);
+				return;
 			}
 		}
-		
-		// update shape position, forces, speed
-		if (lastMovementCollision == null)
-			if (rolling)
-				doRoll(dt);
-			else
-				doFly(dt, distance);
-		else
-			if (rolling)
-				doRollCollision(dt);
-			else
-				doFlyCollision(dt);
 	}
 
 	public void reset() {
 		airFriction.setDirection(0, 0, 0);
-		mTrace.reset();
+		trace.reset();
 		mShape.moveTo(Settings.getBallStartPosition());
 		speed.setDirection(Settings.getBallStartSpeed()); // watch out with new
 															// movables...
+		mShape.temperature = 20;
 	}
 
 	private boolean isRolling() {
 		// rolling if a collision with a "floor"-object happened last round and
 		// the speed away from the floor is small
-		return lastMovementCollision != null
-				&& ((Quadrilateral) lastMovementCollision.collidedSolid)
+		return mLastMovementCollision != null
+				&& ((Quadrilateral) mLastMovementCollision.collidedSolid)
 						.getNonZeroDimension() == 1 && speed.getY() < 0.1f;
 	}
-
-	private void doRoll(final float dt) {
-		// // if (isRolling){
-		// // speed.setDirection(new Vector(speed.getX() + acceleration.getX() *
-		// dt, 0, speed.getZ() + acceleration.getZ() * dt));
-		// // distance.setDirection(distance.getX(), mShape.location.getY(),
-		// distance.getZ());
-		// //
-		// // Log.e(TAG, "rolling");
-		// // }else{
-		// // reset variables
-
-	}
-
-	private void doRollCollision(final float dt) {
-
-	}
-
-	private void doFly(final float dt, final IVector distance) {
-		// move shape
-		mShape.moveTo(mShape.location.add(distance));
-		
-		// set forces
-		airFriction.setDirection(speed.getNormalizedVector().multiply(
-				-((Ball)mShape).getDragFactor() * speed.getLength() * speed.getLength()));
-		
-		// calculate acceleration
-		final IVector acceleration = airFriction.add(gravitation);
-		
-		// update speed
-		speed.setDirection(speed.add(acceleration.multiply(dt)));
-	}
-
-	private void doFlyCollision(final float dt) {
-		// go to collision point
-		doFly(dt * lastMovementCollision.travelPercentage, lastMovementCollision.collisionPoint.add(mShape.location.multiply(-1)));
-		
-		// do collision stuff
-		MovementEngine.playSound(lastMovementCollision.collidedSolid.tag);
-		
-		final float curDt = dt * lastMovementCollision.travelPercentage;
-		final float newDt = dt - curDt;
-		
-		// calculate ausfallswinkel (= einfallswinkel, must change that)
-		final IVector n = lastMovementCollision.solidNormalVector.getNormalizedVector();
-		
-		final IVector newSpeed = speed.add(n.multiply(-2 * speed.multiply(n))).multiply(Settings.getCoefficientOfRestitution()); // formula for ausfallswinkel
-		
-		speed.setDirection(newSpeed);
-		
-		// do rest of movement
-		final IVector newDistance = speed.multiply(newDt);
-		
-		doFly(newDt, newDistance);
-	}
-
 
 	public float getPotentialEnergy(){
 		return mShape.location.getY() * gravitation.getLength();
@@ -200,7 +207,7 @@ public class Movable {
 	}
 	
 	public float getThermicEnergy(){
-		return 0;
+		return SPECIFIC_WARMTH_CAPACITY * mShape.temperature;
 	}
 	
 	public float getTotalEnergy(){
