353152e21e0013c385b7a12e6493b0fc8f871217
Fabian Furger <furgerfabian@hotmail.com>
2015-01-15 20:55:02 +0000

    major UI changes inc. displaying xml-layout rather than squashview

--- Squash Simulation/src/ch/squash/simulation/main/SquashActivity.java
+++ Squash Simulation/src/ch/squash/simulation/main/SquashActivity.java
@@ -5,26 +5,20 @@ import java.util.Set;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.util.Log;
 import android.view.GestureDetector.OnDoubleTapListener;
 import android.view.GestureDetector.OnGestureListener;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.Window;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import ch.squash.simulation.R;
 import ch.squash.simulation.common.Settings;
 import ch.squash.simulation.graphic.Shader;
 import ch.squash.simulation.graphic.SquashRenderer;
-import ch.squash.simulation.shapes.common.IVector;
-import ch.squash.simulation.shapes.shapes.Ball;
 
 
 public class SquashActivity extends Activity implements OnGestureListener, OnDoubleTapListener {
@@ -34,16 +28,7 @@ public class SquashActivity extends Activity implements OnGestureListener, OnDou
 
 	// constants
 	private final static int RESULT_SETTINGS = 1;
-	private final static int UI_UPDATE_INTERVAL = 200; // ms
 	
-	// views
-	private SquashView mSquashView;
-	private TextView mTxtHudFps;
-	private TextView mTxtHudBall;
-	
-	// misc
-	private boolean mIsUpdateUi = true;
-
     private GestureDetectorCompat mDetector; 
 
 	@Override
@@ -56,10 +41,6 @@ public class SquashActivity extends Activity implements OnGestureListener, OnDou
         mDetector = new GestureDetectorCompat(this,this);
         mDetector.setOnDoubleTapListener(this);
 
-		// ensure the camera is not rotating on startup
-//		if (Settings.getCameraMode() == 0)
-//			Settings.setCameraMode(1);
-
 		// ensure that if nothing would be drawn, ball court and forces are
 		// drawn instead
 		if (Settings.getVisibleObjectCollections().size() == 0) {
@@ -70,23 +51,11 @@ public class SquashActivity extends Activity implements OnGestureListener, OnDou
 			Settings.setVisibleObjectCollections(ss);
 		}
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		mTxtHudFps = new TextView(this);
-		mTxtHudFps.setTextColor(Color.RED);
-		mTxtHudFps.setGravity(Gravity.END);
-		mTxtHudBall = new TextView(this);
-		mTxtHudBall.setTextColor(Color.BLUE);
-		mTxtHudBall.setGravity(Gravity.START);
-
-		mSquashView = new SquashView(this);
-
-		setContentView(mSquashView);
-		final FrameLayout parent = (FrameLayout) mSquashView.getParent();
-		parent.addView(mTxtHudFps);
-		parent.addView(mTxtHudBall);
-
-		updateUi();
+//		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
+		setContentView(R.layout.layout_main);
+		SquashView.getInstance().registerViews((TextView)findViewById(R.id.txtHudFps), (TextView)findViewById(R.id.txtHudBall));
+
 		Log.i(TAG, "SquashActivity created");
 	}
 
@@ -100,7 +69,7 @@ public class SquashActivity extends Activity implements OnGestureListener, OnDou
 	@Override
 	public boolean onOptionsItemSelected(final MenuItem item) {
 		if (item.getItemId() == R.id.menu_settings) {
-			mSquashView.onPause();
+			SquashView.getInstance().onPause();
 
 			final Intent i = new Intent(this, SettingsActivity.class);
 			startActivityForResult(i, RESULT_SETTINGS);
@@ -116,66 +85,27 @@ public class SquashActivity extends Activity implements OnGestureListener, OnDou
 		super.onActivityResult(requestCode, resultCode, data);
 
 		if (requestCode == RESULT_SETTINGS) {
-			mSquashView.onResume();
+			SquashView.getInstance().onResume();
 			Log.d(TAG, "SettingsActivity finished");
 		}
 	}
 
 	@Override
 	protected void onPause() {
-		mSquashView.onPause();
+		SquashView.getInstance().onPause();
 		super.onPause();
-		mIsUpdateUi = false;
 		Shader.destroyShaders();	// destroy shaders so that new ones will be created
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		mSquashView.onResume();
-		mInstance.updateUi();
+		SquashView.getInstance().onResume();
 	}
 
-	private void updateUi() {
-		mIsUpdateUi = true;
-		
-		new Thread() {
-			@Override
-			public void run() {
-				while (mIsUpdateUi) {
-					mInstance.runOnUiThread(new Runnable() {
-						@Override
-						public void run() {
-							// update *ps
-							mTxtHudFps.setText(String.format("%.2f", SquashRenderer.getFps()) + " fps\n" 
-									+ String.format("%.2f", MovementEngine.getMps()) + " mps");
-							// update ball
-							final Ball ball = SquashRenderer.getSquashBall();
-							final IVector location =  ball.getLocation();
-							final String locationString = "Location:\t" + String.format("%.2f", location.getX()) + "/" + String.format("%.2f", location.getY()) + "/" + String.format("%.2f", location.getZ());
-							final IVector speed =  ball.getMovable().speed;
-							final String speedString = "Speed:\t\t\t" + String.format("%.2f", speed.getX()) + "/" + String.format("%.2f", speed.getY()) + "/" + String.format("%.2f", speed.getZ());
-							mTxtHudBall.setText(locationString + "\n" + speedString);
-						}
-					});
-
-					try {
-						Thread.sleep(UI_UPDATE_INTERVAL);
-					} catch (InterruptedException e) {
-						Log.e(TAG, "Error while sleepint", e);
-					}
-				}
-			}
-		}.start();
-	}
-	
 	public static SquashActivity getInstance() {
 		return mInstance;
 	}
-	
-	
-	
-	
 	
 	
     @Override 
--- Squash Simulation/src/ch/squash/simulation/main/SquashView.java
+++ Squash Simulation/src/ch/squash/simulation/main/SquashView.java
@@ -2,28 +2,99 @@ package ch.squash.simulation.main;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
+import android.widget.TextView;
 import ch.squash.simulation.graphic.SquashRenderer;
+import ch.squash.simulation.shapes.common.IVector;
+import ch.squash.simulation.shapes.shapes.Ball;
 
 public class SquashView extends GLSurfaceView implements SurfaceHolder.Callback {
 	private final static String TAG = SquashView.class.getSimpleName();
 
-	public SquashView(final Context context) {
-		super(context);
+	private static SquashView mInstance;
 
+	// views
+	private TextView mTxtHudFps;
+	private TextView mTxtHudBall;
+
+	private final static int UI_UPDATE_INTERVAL = 200; // ms
+	
+	// misc
+	private boolean mIsUpdateUi = true;
+
+	
+	public static SquashView getInstance(){
+		return mInstance;
+	}
+	
+	public SquashView(Context context, AttributeSet attrs)
+	{
+	   super(context, attrs);
+
+		mInstance = this;
+		
 		setEGLContextClientVersion(2);
 
 		setRenderer(SquashRenderer.getInstance());
 
 		setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
-		
+
 		Log.i(TAG, "SquashView created");
+	}
+	
+	public void registerViews(final TextView hudFps, final TextView hudBall){
+		mTxtHudFps = hudFps;
+		mTxtHudBall = hudBall;
+		
+		updateUi();
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
 		MovementEngine.pause();
+		
+		mIsUpdateUi = false;
+	}
+	
+	@Override
+	public void onResume(){
+		super.onResume();
+		mInstance.updateUi();
+	}
+
+	private void updateUi() {
+		mIsUpdateUi = true;
+		
+		new Thread() {
+			@Override
+			public void run() {
+				while (mIsUpdateUi) {
+					SquashActivity.getInstance().runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							// update *ps
+							mTxtHudFps.setText(String.format("%.2f", SquashRenderer.getFps()) + " fps\n" 
+									+ String.format("%.2f", MovementEngine.getMps()) + " mps");
+							// update ball
+							final Ball ball = SquashRenderer.getSquashBall();
+							final IVector location =  ball.getLocation();
+							final String locationString = "Location:\t" + String.format("%.2f", location.getX()) + "/" + String.format("%.2f", location.getY()) + "/" + String.format("%.2f", location.getZ());
+							final IVector speed =  ball.getMovable().speed;
+							final String speedString = "Speed:\t\t\t" + String.format("%.2f", speed.getX()) + "/" + String.format("%.2f", speed.getY()) + "/" + String.format("%.2f", speed.getZ());
+							mTxtHudBall.setText(locationString + "\n" + speedString);
+						}
+					});
+
+					try {
+						Thread.sleep(UI_UPDATE_INTERVAL);
+					} catch (InterruptedException e) {
+						Log.e(TAG, "Error while sleepint", e);
+					}
+				}
+			}
+		}.start();
 	}
 }
