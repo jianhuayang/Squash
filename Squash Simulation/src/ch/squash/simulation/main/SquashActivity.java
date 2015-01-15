94142d0e00c804431541c7e241da334d5d5de384
Fabian Furger <furgerfabian@hotmail.com>
2015-01-15 11:40:06 +0000

    adjustment to gesture recognition

--- Squash Simulation/src/ch/squash/simulation/main/SquashActivity.java
+++ Squash Simulation/src/ch/squash/simulation/main/SquashActivity.java
@@ -7,13 +7,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.util.Log;
+import android.view.GestureDetector.OnDoubleTapListener;
+import android.view.GestureDetector.OnGestureListener;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import ch.squash.simulation.R;
 import ch.squash.simulation.common.Settings;
 import ch.squash.simulation.graphic.Shader;
@@ -21,7 +26,8 @@ import ch.squash.simulation.graphic.SquashRenderer;
 import ch.squash.simulation.shapes.common.IVector;
 import ch.squash.simulation.shapes.shapes.Ball;
 
-public class SquashActivity extends Activity {
+
+public class SquashActivity extends Activity implements OnGestureListener, OnDoubleTapListener {
 	// static
 	private final static String TAG = SquashActivity.class.getSimpleName();
 	private static SquashActivity mInstance;
@@ -37,12 +43,18 @@ public class SquashActivity extends Activity {
 	
 	// misc
 	private boolean mIsUpdateUi = true;
-	
+
+    private GestureDetectorCompat mDetector; 
+
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		mInstance = this;
+		
+		// gesture stuff
+        mDetector = new GestureDetectorCompat(this,this);
+        mDetector.setOnDoubleTapListener(this);
 
 		// ensure the camera is not rotating on startup
 //		if (Settings.getCameraMode() == 0)
@@ -160,4 +172,80 @@ public class SquashActivity extends Activity {
 	public static SquashActivity getInstance() {
 		return mInstance;
 	}
+	
+	
+	
+	
+	
+	
+    @Override 
+    public boolean onTouchEvent(MotionEvent event){ 
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) { 
+//        Log.d(TAG,"onDown: " + event.toString()); 
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2, 
+            float velocityX, float velocityY) {
+//        Log.d(TAG, "onFling: " + event1.toString()+event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+//        Log.d(TAG, "onLongPress: " + event.toString()); 
+
+    	MovementEngine.pause();
+    	MovementEngine.resetMovables();
+    	
+    	Toast.makeText(this, "Movables reset", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+            float distanceY) {
+//        Log.d(TAG, "onScroll: " + e1.toString()+e2.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+//        Log.d(TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+//        Log.d(TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d(TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+//        Log.d(TAG, "onDoubleTapEvent: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+//        Log.d(TAG, "onSingleTapConfirmed: " + event.toString());
+
+    	Toast.makeText(this,
+    			"MovementEngine " + (MovementEngine.isRunning() ? "stopped" : "started"), Toast.LENGTH_SHORT).show();
+		MovementEngine.toggleRunning();
+    	
+        return true;
+    }
 }
--- Squash Simulation/src/ch/squash/simulation/main/SquashView.java
+++ Squash Simulation/src/ch/squash/simulation/main/SquashView.java
@@ -1,21 +1,14 @@
 package ch.squash.simulation.main;
 
-import ch.squash.simulation.graphic.SquashRenderer;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.SurfaceHolder;
-import android.widget.Toast;
+import ch.squash.simulation.graphic.SquashRenderer;
 
 public class SquashView extends GLSurfaceView implements SurfaceHolder.Callback {
 	private final static String TAG = SquashView.class.getSimpleName();
 
-	private final GestureDetector mGestureDetector;
-	
-	private static boolean mLongPress;
-	
 	public SquashView(final Context context) {
 		super(context);
 
@@ -24,17 +17,6 @@ public class SquashView extends GLSurfaceView implements SurfaceHolder.Callback 
 		setRenderer(SquashRenderer.getInstance());
 
 		setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
-
-		mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
-		    public void onLongPress(final MotionEvent e) {
-		    	MovementEngine.pause();
-		    	MovementEngine.resetMovables();
-		    	
-		    	Toast.makeText(SquashActivity.getInstance(), "Movables reset", Toast.LENGTH_SHORT).show();
-		    	
-		        mLongPress = true;
-		    }
-		});
 		
 		Log.i(TAG, "SquashView created");
 	}
@@ -45,18 +27,18 @@ public class SquashView extends GLSurfaceView implements SurfaceHolder.Callback 
 		MovementEngine.pause();
 	}
 
-	public boolean onTouchEvent(final MotionEvent event) {
-		mGestureDetector.onTouchEvent(event);
+//	public boolean onTouchEvent(final MotionEvent event) {
+//		mGestureDetector.onTouchEvent(event);
 		
-		if (mLongPress){
-			// only reset longpress flag if it was completed (if the action is UP)
-			mLongPress = event.getAction() != MotionEvent.ACTION_UP;
-		} else if (event.getAction() == MotionEvent.ACTION_UP){
-	    	Toast.makeText(SquashActivity.getInstance(),
-	    			"MovementEngine " + (MovementEngine.isRunning() ? "stopped" : "started"), Toast.LENGTH_SHORT).show();
-			MovementEngine.toggleRunning();
-		}
+//		if (mLongPress){
+//			// only reset longpress flag if it was completed (if the action is UP)
+//			mLongPress = event.getAction() != MotionEvent.ACTION_UP;
+//		} else if (event.getAction() == MotionEvent.ACTION_UP){
+//	    	Toast.makeText(SquashActivity.getInstance(),
+//	    			"MovementEngine " + (MovementEngine.isRunning() ? "stopped" : "started"), Toast.LENGTH_SHORT).show();
+//			MovementEngine.toggleRunning();
+//		}
 		
-		return true;
-	}
+//		return true;
+//	}
 }
